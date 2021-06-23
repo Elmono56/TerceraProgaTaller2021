00f1bc65b3ec6176bd4a94312c6c1a40d6ad3003
Elmono56 <78763533+Elmono56@users.noreply.github.com>
2021-06-23 11:34:24 -0600

    form2
    creación form2 config

---
+++ form2 config.py
@@ -0,0 +1,14 @@
+#menu configuracion
+
+#librerías
+import tkinter
+
+#gráficos
+
+config=tkinter.Tk()
+config.geometry("500x500")
+config.title("Juego Futoshiki - Configuración")
+
+blanco=tkinter.Label(config,text="     ")
+blanco.grid(row=0,column=0)
+
