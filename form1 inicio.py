41bc2e40958c1eeafcfe90d3b54d704a8b81a29f
Elmono56 <78763533+Elmono56@users.noreply.github.com>
2021-06-23 11:05:41 -0600

    form1
    creación form1

---
+++ form1 inicio.py
@@ -0,0 +1,11 @@
+#menu inicio
+
+#librerías
+import tkinter
+from tkinter import messagebox
+
+#gráficos
+
+inicio=tkinter.Tk()
+inicio.geometry("500x500")
+inicio.title("Juego Futoshiki - Menú de Inicio")
