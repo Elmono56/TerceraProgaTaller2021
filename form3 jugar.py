12728c5ea3a00a0389aa3bcdf95b73fc0ed6da87
Elmono56 <78763533+Elmono56@users.noreply.github.com>
2021-06-24 22:53:04 -0600

    creacion form3
    creacion form 3 juego

---
+++ form3 jugar.py
@@ -0,0 +1,17 @@
+#menu juego
+
+#librerías
+import tkinter
+from tkinter import messagebox
+
+#funciones aux
+
+#gráficos
+
+def jugar():
+    juego=tkinter.Tk()
+    juego.geometry("700x400")
+    juego.title("Juego Futoshiki - Jugar")
+
+
+jugar()
